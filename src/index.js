9d7e36e4851fd521f781a3e1f61b97328502f2a9
a.petrakovich <petrakovich413@gmail.com>
2017-09-29 20:10:41 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let numbers = [];
+
+  first = first.split('').reverse();
+  second = second.split('').reverse();
+
+  for (let i = 0; first[i] >= 0; i++) {
+    for (let j = 0; second[j] >= 0; j++) {
+      if (!numbers[i + j]) {
+        numbers[i + j] = 0;
+      }
+      numbers[i + j] += first[i] * second[j];
+    }
+  }
+
+  for (let i = 0; numbers[i] >= 0; i++) {
+    if (numbers[i] >= 10) {
+      if (!numbers[i + 1]) {
+        numbers[i + 1] = 0;
+      } 
+      numbers[i + 1] += parseInt(numbers[i] / 10);
+      numbers[i] %= 10;
+    }
+  }
+
+ 
+  return  numbers.reverse().join('');
 }
